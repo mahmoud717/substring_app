e691a926f432de9d961137617878cac64c2672b6
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-06-29 01:57:03 +0200

    implemented the algo

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,18 +1,18 @@
 class ResultsController < ApplicationController
   include ApplicationHelper 
+  include ResultsHelper
   before_action :logged_in?
-  def new
+
+  def new 
     @result = Result.new
   end
 
   def create
     @get_result = Result.find_by(full_string: params[:full_string], user_id: session[:user_id])
-    if @get_result
-      redirect_to result_path(params[:full_string])
-      return
-    end
+    redirect_to result_path(params[:full_string]) and return if @get_result
 
-    @new_result = Result.new(full_string: params[:full_string], user_id: session[:user_id])
+    @sub = LongestSubstring(params[:full_string])
+    @new_result = Result.new(full_string: params[:full_string], longest_sub: @sub, longest_sub_length: @sub.length, user_id: session[:user_id])
       if @new_result.save
         p @new_result.errors.full_messages
         redirect_to result_path(params[:full_string])
