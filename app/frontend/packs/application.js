5f19ca6319e9d573b3bb72e218a9d44261f8e435
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-06-29 02:36:19 +0200

    installed bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,4 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import '../js/bootstrap_js_files.js'
